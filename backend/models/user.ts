3281dc965cda61d1637df546be558c1a168d55c3
JanKulesza <kulesz.jasiek@gmail.com>
2025-04-09 09:24:14 +0200

    Hash passwords

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import { deleteImage } from "../utils/cloudinary.ts";
 
 const userSchema = new mongoose.Schema({
@@ -32,6 +33,26 @@ userSchema.methods.toJSON = function () {
   delete user.password;
   return user;
 };
+
+userSchema.pre("save", async function (next) {
+  const password = this.get("password");
+  if (password) {
+    this.set("password", await bcrypt.hash(password, 10));
+  }
+  next();
+});
+
+userSchema.pre("findOneAndUpdate", async function (next) {
+  const update = this.getUpdate() as mongoose.UpdateQuery<unknown>;
+
+  if (update.password) {
+    update.password = await bcrypt.hash(update.password, 10);
+  } else if (update.$set?.password) {
+    update.$set.password = await bcrypt.hash(update.$set.password, 10);
+  }
+
+  next();
+});
 
 userSchema.pre(["findOneAndDelete", "findOneAndUpdate"], async function (next) {
   const doc = await this.model.findOne(this.getQuery());
