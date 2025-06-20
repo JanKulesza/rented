fa1c418e7dd03a74a9482f8474ed0cdb7d5469a8
JanKulesza <kulesz.jasiek@gmail.com>
2025-06-20 21:14:49 +0200

    Fix responsiveness

--- frontend/components/app/listings/add-property/add-property.tsx
+++ frontend/components/app/listings/add-property/add-property.tsx
@@ -250,7 +250,7 @@ const AddProperty = ({
             </>
           ) : (
             <>
-              <SquarePen /> Edit Property
+              <SquarePen /> Edit
             </>
           )}
         </Button>
